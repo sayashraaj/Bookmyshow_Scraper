7d728249982076043f6f4f38e1822b982b86d058
sayash <sayashraaj@gmail.com>
2019-12-19 17:35:28 +0530

    updated bms.py,url.txt,urlsupply.py

--- bookmyshow/bookmyshow/spiders/bookmyshow.py
+++ bookmyshow/bookmyshow/spiders/bookmyshow.py
@@ -3,6 +3,7 @@ from ..items import BookmyshowItem
 
 class spider(scrapy.Spider):
     name='bms'
+#Reading urls.txt for obtaining URLs
     f = open("urls.txt")
     start_urls = [url.strip() for url in f.readlines()]
     f.close()
@@ -10,14 +11,14 @@ class spider(scrapy.Spider):
     def parse(self,response):
         a = response.css('div.wrapper li.list')
         items = BookmyshowItem()
-
+#Crawling and storing output for each time the item is found
         for b in a:
             namecin = b.css('::attr(data-name)').extract()
             timin = b.css('a::attr(data-display-showtime)').extract()
             percentage = b.css('a::attr(data-seats-percent)').extract()
             date = b.css('a::attr(data-cut-off-date-time)').extract()
             sr = b.css('::attr(data-sub-region-name)').extract()
-
+#Storing the data using items.py
             items['cinema'] = namecin
             items['subregion'] = sr
             items['timings'] = timin
--- bookmyshow/bookmyshow/urlsupply.py
+++ bookmyshow/bookmyshow/urlsupply.py
@@ -1,22 +1,34 @@
 from datetime import date
+#Finding path
 import os.path
 current_directory = os.path.dirname(__file__)
 parent_directory = os.path.split(current_directory)[0]
 file=open(parent_directory+"/urls.txt", "a")
-
+#Getting system date
 today=date.today()
 date= int(today.strftime("%Y%m%d"))
 
 count=int(0)
+#Input URLs
 with open("url.txt") as f:
     # Content_list contains the read lines
     content_list = f.readlines()
-    count+=1
 
-for j in range(0,count):
+f = "url.txt"
+with open(f, 'r') as f:
+    for line in f:
+        count += 1.
 
-    for i in range(0,7):
+print(content_list) #To test if content_list is giving proper output
+
+for j in range(0,int(count)):
+
+    for i in range(0,4):
         temp=str(int(today.strftime("%Y%m")))+str(int(today.strftime("%d"))+i);
-        file.write(str(content_list[j])+(temp)+"\n")
 
-    file.close()
+        url=str(content_list[j])
+        print(url[:-9])
+        file.write((url[:-9])+(temp)+"\n")
+
+
+file.close()
